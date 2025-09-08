548f2ea3cba2519e403ad4a98a95f0bb8f834f68
mahdi alin <mahadaalin@gmail.com>
2025-09-08 10:47:16 -0700

    Commit 2
    I imported javax.swing.* to use GUI components like JFrame, JButton, etc.
     I imported java.awt.* to use layout managers and basic UI tools.

--- untitled/src/MusicGenreApp.java
+++ untitled/src/MusicGenreApp.java
@@ -4,5 +4,8 @@ import java.awt.*;
 public class MusicGenreApp {
 
     public void metod() {
+        JFrame frame = new JFrame("Music Genre Interface");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
