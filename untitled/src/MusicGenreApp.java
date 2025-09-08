d8a45d3ae6d280da8488da6a1b3af665a3e04d09
mahdi alin <mahadaalin@gmail.com>
2025-09-08 10:49:20 -0700

    Commit 1
     I imported javax.swing.* to use GUI components like JFrame, JButton, etc.
    I imported java.awt.* to use layout managers and basic UI tools.
    I created a public class named MusicGenreApp as the main application class.
     I added a public method called metod() as a placeholder for future GUI logic.

--- untitled/src/MusicGenreApp.java
+++ untitled/src/MusicGenreApp.java
@@ -6,6 +6,9 @@ public class MusicGenreApp {
     public void metod() {
         JFrame frame = new JFrame("Music Genre Interface");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        frame.setSize(400, 300);
+        frame.setLayout(new BorderLayout());
+        frame.setLocationRelativeTo(null);
+        JLabel titleLabel = new JLabel("My Music Genre", JLabel.CENTER);
     }
 }
